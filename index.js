2cf8c7ec1bfb5b56474b901d6fc923af3ebf0e31
An-Shiang Tsai <ast850328@gmail.com>
2020-05-23 14:59:22 +0800

    Add: rules

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+'use strict';
 require('dotenv').config();
+var rules = require('./rules.json');
 var TelegramBot = require('node-telegram-bot-api');
 var bot = new TelegramBot(process.env.TOKEN, { polling: true });
 bot.on('message', function (msg) {
@@ -6,31 +8,14 @@ bot.on('message', function (msg) {
     var msgId = msg.message_id;
     var message = msg.text;
     var response = '';
-    if (message == '/start') {
-        response = '您好，歡迎來取暖，我爛';
+    for (var _i = 0, _a = Object.keys(rules); _i < _a.length; _i++) {
+        var key = _a[_i];
+        if (message === key) {
+            response = rules[key];
+            break;
+        }
     }
-    else if (message == '/help') {
-        response = '我可以為您取暖';
-    }
-    else if (message.includes('我爛')) {
-        response = '我才爛';
-    }
-    else if (message.includes('我才爛')) {
-        response = '我更爛';
-    }
-    else if (message.includes('我更爛')) {
-        response = '我超爛';
-    }
-    else if (message.includes('我超爛')) {
-        response = '我最爛';
-    }
-    else if (message.includes('我最爛')) {
-        response = '我比你爛';
-    }
-    else if (message.includes('我比你爛')) {
-        response = '我比你更爛';
-    }
-    else {
+    if (response === '') {
         response = '我爛';
     }
     bot.sendMessage(chatId, response, { reply_to_message_id: msgId });
--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-require('dotenv').config()
+'use strict';
+require('dotenv').config();
+const rules = require('./rules.json');
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TOKEN, {polling: true});
 
@@ -6,25 +8,16 @@ bot.on('message', (msg: any) => {
   const chatId = msg.chat.id;
   const msgId = msg.message_id;
   let message = msg.text;
-  let response = ''
-  if (message == '/start') {
-    response = '您好，歡迎來取暖，我爛'
-  } else if (message == '/help') {
-    response = '我可以為您取暖'
-  } else if (message.includes('我爛')) {
-    response = '我才爛';
-  } else if (message.includes('我才爛')) {
-    response = '我更爛';
-  } else if (message.includes('我更爛')) {
-    response = '我超爛';
-  } else if (message.includes('我超爛')){
-    response = '我最爛';
-  } else if (message.includes('我最爛')){
-    response = '我比你爛';
-  } else if (message.includes('我比你爛')){
-    response = '我比你更爛';
-  } else {
+  let response = '';
+  for (let key of Object.keys(rules)) {
+    if (message === key) {
+      response = rules[key];
+      break;
+    }
+  }
+  if (response === '') {
     response = '我爛';
   }
+
   bot.sendMessage(chatId, response, {reply_to_message_id: msgId});
 });
